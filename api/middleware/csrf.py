9250532f519b80cb96c5255bdda78d6bd3d96ce8
Muhammad Zaid Amjad <zaidamjadse@gmail.com>
2025-04-20 19:15:58 +0500

    CSRF Disable

--- api/middleware/csrf.py
+++ api/middleware/csrf.py
@@ -6,13 +6,12 @@ class CustomCsrfMiddleware(CsrfViewMiddleware):
     def process_view(self, request, callback, callback_args, callback_kwargs):
         # Check if the path starts with any of the exempt paths
         if any(request.path.startswith(path) for path in settings.CSRF_EXEMPT_PATHS):
+            setattr(request, '_dont_enforce_csrf_checks', True)  # Completely disable CSRF
             return None
-            
-        # For non-exempt paths, process normally
         return super().process_view(request, callback, callback_args, callback_kwargs)
 
     def process_request(self, request):
-        # Skip CSRF checks for exempt paths
         if any(request.path.startswith(path) for path in settings.CSRF_EXEMPT_PATHS):
+            setattr(request, '_dont_enforce_csrf_checks', True)  # Completely disable CSRF
             return None
         return super().process_request(request) 
