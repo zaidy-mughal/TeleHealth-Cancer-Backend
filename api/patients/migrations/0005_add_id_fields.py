276a8b9a8cab22c0ac79a30b741d873998631ea1
Muhammad Zaid Amjad <zaidamjadse@gmail.com>
2025-04-24 02:59:11 +0500

    fix: update migration to add id fields first

--- api/patients/migrations/0005_add_id_fields.py
+++ api/patients/migrations/0005_add_id_fields.py
@@ -19,5 +19,60 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.AddField(
+            model_name='patient',
+            name='id',
+            field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True),
+        ),
+        migrations.AddField(
+            model_name='iodineallergy',
+            name='id',
+            field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True),
+        ),
+        migrations.AddField(
+            model_name='allergy',
+            name='id',
+            field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True),
+        ),
+        migrations.AddField(
+            model_name='medication',
+            name='id',
+            field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True),
+        ),
+        migrations.AddField(
+            model_name='medicalhistory',
+            name='id',
+            field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True),
+        ),
+        migrations.AddField(
+            model_name='surgicalhistory',
+            name='id',
+            field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True),
+        ),
+        migrations.AddField(
+            model_name='cancertype',
+            name='id',
+            field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True),
+        ),
+        migrations.AddField(
+            model_name='cancerhistory',
+            name='id',
+            field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True),
+        ),
+        migrations.AddField(
+            model_name='addictionhistory',
+            name='id',
+            field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True),
+        ),
+        migrations.AddField(
+            model_name='primaryphysician',
+            name='id',
+            field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True),
+        ),
+        migrations.AddField(
+            model_name='pharmacist',
+            name='id',
+            field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True),
+        ),
         migrations.RunPython(gen_uuid),
     ] 
