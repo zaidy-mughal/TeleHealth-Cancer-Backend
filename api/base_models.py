e60b85494d22bfb3152d27773b595a92b2302a3d
Muhammad Zaid Amjad <zaidamjadse@gmail.com>
2025-04-24 02:29:06 +0500

    fix: remove uuid field from TimeStampMixin to use existing id column

--- api/base_models.py
+++ api/base_models.py
@@ -5,7 +5,6 @@ def get_uuid():
     return uuid.uuid4()
 
 class TimeStampMixin(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
