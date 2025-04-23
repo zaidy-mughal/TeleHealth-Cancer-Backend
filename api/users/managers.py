203f9d8bb9f0bbae447b30e50efbe172d92f88c5
Muhammad Zaid Amjad <zaidamjadse@gmail.com>
2025-04-24 02:59:45 +0500

    fix: update all models and migrations

--- api/users/managers.py
+++ api/users/managers.py
@@ -5,20 +5,22 @@ from django.utils.translation import gettext_lazy as _
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, email, password, **extra_fields):
+    def _create_user(self, email, username, password, **extra_fields):
         """
-        Create and save a user with the given email, and password.
+        Create and save a user with the given email, username, and password.
         """
         if not email:
             raise ValueError('The given email must be set')
+        if not username:
+            raise ValueError('The given username must be set')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email=None, password=None, **extra_fields):
+    def create_user(self, email, username, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         if extra_fields.get('is_staff') is True:
@@ -26,9 +28,9 @@ class UserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is True:
             raise ValueError('Simple user must have is_superuser=False.')
 
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, username, password, **extra_fields)
 
-    def create_staffuser(self, email, password, **extra_fields):
+    def create_staffuser(self, email, username, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', False)
 
@@ -37,9 +39,9 @@ class UserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is True:
             raise ValueError('Staff user must have is_superuser=False.')
 
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, username, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email, username, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -48,4 +50,4 @@ class UserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, username, password, **extra_fields)
