d2ae9487b35076918fb682605af8e008350ca13f
Muhammad Zaid Amjad <zaidamjadse@gmail.com>
2025-04-24 02:43:23 +0500

    fix: update User model to use id instead of TimeStampMixin

--- api/users/models.py
+++ api/users/models.py
@@ -7,42 +7,37 @@ from django.utils import timezone
 from django.contrib.auth.models import UnicodeUsernameValidator
 from api.users.choices import Role
 
-from api.base_models import TimeStampMixin
 from api.users.managers import UserManager
 
 
-
-class User(AbstractBaseUser, PermissionsMixin, TimeStampMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     """
     An abstract base class implementing a fully featured User model with
     admin-compliant permissions.
 
-    Email, uuid and password are required. Other fields are optional.
+    Email and password are required. Other fields are optional.
     """
 
     username_validator = UnicodeUsernameValidator()
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(
         _('username'),
         max_length=150,
         unique=True,
-        null=True,
-        blank=True,
-        help_text=_('Optional. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
         validators=[username_validator],
         error_messages={
             'unique': _("A user with that username already exists."),
         },
     )
-    email = models.CharField(
+    email = models.EmailField(
         _('email'),
-        max_length=150,
         unique=True,
-        help_text=_(
-            'Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
-        validators=[username_validator, validate_email],
+        help_text=_('Required. A valid email address.'),
+        validators=[validate_email],
         error_messages={
-            'unique': _("A user with that username already exists."),
+            'unique': _("A user with that email address already exists."),
         },
     )
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
@@ -62,13 +57,13 @@ class User(AbstractBaseUser, PermissionsMixin, TimeStampMixin):
             'Unselect this instead of deleting accounts.'
         ),
     )
-    last_active = models.DateTimeField(_('last_active'), null=True, blank=True)
+    last_active = models.DateTimeField(_('last_active'), default=timezone.now)
     date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    USERNAME_FIELD = EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     objects = UserManager()
 
@@ -76,6 +71,12 @@ class User(AbstractBaseUser, PermissionsMixin, TimeStampMixin):
         verbose_name = _('user')
         verbose_name_plural = _('users')
 
+    def __str__(self):
+        return self.email
+
     def get_full_name(self):
         full_name = f'{self.first_name} {self.last_name}'
         return full_name.strip()
+
+    def get_short_name(self):
+        return self.first_name
