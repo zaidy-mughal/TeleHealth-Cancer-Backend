198de119913dcda25a28c1eed6e3f11e623603c5
Muhammad Zaid Amjad <zaidamjadse@gmail.com>
2025-04-24 17:43:06 +0500

    Configure email settings for SSL/465

--- config/settings.py
+++ config/settings.py
@@ -352,10 +352,11 @@ REST_FRAMEWORK = {
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_PORT = env.int('EMAIL_PORT')
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = env('EMAIL_USE_TLS', default=True)
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=False)
+EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL', default=False)
 DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
 
 # ... existing code ... 
