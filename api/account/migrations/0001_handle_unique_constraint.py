061f25a0949b003517d3936b31e7296e50ce95cb
Muhammad Zaid Amjad <zaidamjadse@gmail.com>
2025-04-24 09:36:57 +0500

    fix: skip problematic unique constraint migration

--- api/account/migrations/0001_handle_unique_constraint.py
+++ api/account/migrations/0001_handle_unique_constraint.py
@@ -1,4 +1,7 @@
+# Generated manually
+
 from django.db import migrations
+
 
 class Migration(migrations.Migration):
 
@@ -8,19 +11,8 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunSQL(
-            """
-            DO $$
-            BEGIN
-                IF EXISTS (
-                    SELECT 1
-                    FROM pg_constraint
-                    WHERE conname = 'account_emailaddress_user_id_email_987c8728_uniq'
-                ) THEN
-                    ALTER TABLE account_emailaddress
-                    DROP CONSTRAINT account_emailaddress_user_id_email_987c8728_uniq;
-                END IF;
-            END $$;
-            """,
+            # Skip the problematic migration by doing nothing
+            "",
             reverse_sql="",
         ),
     ]
