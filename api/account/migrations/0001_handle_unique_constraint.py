04d1125db0188d9f3ccf7ef3232364b61fd28e9a
Muhammad Zaid Amjad <zaidamjadse@gmail.com>
2025-04-24 09:34:20 +0500

    fix: update migration

--- api/account/migrations/0001_handle_unique_constraint.py
+++ api/account/migrations/0001_handle_unique_constraint.py
@@ -1,7 +1,4 @@
-# Generated manually
-
 from django.db import migrations
-
 
 class Migration(migrations.Migration):
 
@@ -11,19 +8,19 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunSQL(
-            # Drop the existing unique index directly
             """
             DO $$
             BEGIN
                 IF EXISTS (
                     SELECT 1
-                    FROM pg_indexes
-                    WHERE indexname = 'account_emailaddress_user_id_email_987c8728_uniq'
+                    FROM pg_constraint
+                    WHERE conname = 'account_emailaddress_user_id_email_987c8728_uniq'
                 ) THEN
-                    DROP INDEX account_emailaddress_user_id_email_987c8728_uniq;
+                    ALTER TABLE account_emailaddress
+                    DROP CONSTRAINT account_emailaddress_user_id_email_987c8728_uniq;
                 END IF;
             END $$;
             """,
             reverse_sql="",
         ),
-    ] 
+    ]
