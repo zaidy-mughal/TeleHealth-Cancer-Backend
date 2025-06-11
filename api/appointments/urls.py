c13e445f0380878d5546cf166be520e1e2a51c32
zaidy-mughal <zaidamjadse@gmail.com>
2025-06-11 23:35:08 +0500

    fix(appointment): temporary removed the appointment creation api

--- api/appointments/urls.py
+++ api/appointments/urls.py
@@ -9,7 +9,7 @@ from api.appointments.views import (
 
 
 urlpatterns = [
-    path("create/", AppointmentCreateView.as_view(), name="create-appointment"),
+    # path("create/", AppointmentCreateView.as_view(), name="create-appointment"),
     path("patient/", PatientAppointmentListView.as_view(), name="patient-appointments-list"),
     path("<uuid:uuid>/", AppointmentDetailView.as_view(), name="appointment-detail"),
     path("doctor/", DoctorAppointmentListView.as_view(), name="doctor-appointments-list"),
